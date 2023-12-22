4b49482f19630f53c48535066561cbc886e5f863
Vigneshwaran-PS <psviki29@gmail.com>
2023-12-22 23:29:19 +0530

    List_of_PrimeNumbers is updated

--- Leetcode_Problems/List_of_PrimeNumbers.java
+++ Leetcode_Problems/List_of_PrimeNumbers.java
@@ -1,4 +1,28 @@
 package Leetcode_and_technical_Programs.Leetcode_Problems;
 
 public class List_of_PrimeNumbers {
+    public static void main(String[] args) {
+        int arr[] = {23,1,89,79,69,33,2,0,31,71,43,99,15,30};
+        for(int num : arr){
+            if(num==1 || num==0){
+                continue;
+            }
+            if(prime(num)){
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean prime(int num){
+        int i = 2;
+        int count = 0;
+        while(num > i){
+            if(num % i == 0){
+                count++;
+                break;
+            }
+            i++;
+        }
+        return count==0;
+    }
 }
