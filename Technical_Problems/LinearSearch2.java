66e8a4a1e00e0dd0d0456a460c042665e3ac8e36
Vigneshwaran-PS <psviki29@gmail.com>
2023-12-23 12:27:06 +0530

    LinearSearch2 is created

---
+++ Technical_Problems/LinearSearch2.java
@@ -0,0 +1,27 @@
+package Leetcode_and_technical_Programs.Technical_Problems;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class LinearSearch2 {
+    public static void main(String[] args) {
+        int arr[] = {23,45,57,3,435,21,345,8,23,12};
+        System.out.println(linearSearch(arr,2));
+        System.out.println(linearSearch(arr,23));
+        System.out.println(linearSearch(arr,52));
+        System.out.println(linearSearch(arr,57));
+        System.out.println(linearSearch(arr,12));
+    }
+
+    public static boolean linearSearch(int[] arr, int target){
+
+        if (arr.length == 0) {
+            return false;
+        }
+        for(int num : arr){
+            if(num == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
