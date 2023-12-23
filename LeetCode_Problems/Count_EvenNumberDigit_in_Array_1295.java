f80fc10e54173bcbbe15b2551d1ca5e2f0b99a23
Vigneshwaran-PS <psviki29@gmail.com>
2023-12-23 17:21:48 +0530

    Count_EvenNumberDigit_in_Array_1295 is updated

--- LeetCode_Problems/Count_EvenNumberDigit_in_Array_1295.java
+++ LeetCode_Problems/Count_EvenNumberDigit_in_Array_1295.java
@@ -1,5 +1,45 @@
 package Leetcode_and_technical_Programs.LeetCode_Problems;
 
-public class Count_EvenNumberDigit_in_Array_1295 {
+import java.util.ArrayList;
 
+public class Count_EvenNumberDigit_in_Array_1295 {
+    public static void main(String ar[]){
+        int arr[] ={23,4534,256,1345,39025,3245,1,764};
+
+        // here 23,4534,1345,3245 are even digit because they have even number of digit
+        System.out.println(findNumber(arr));
+    }
+
+    /* this method count the number of even digits available in array */
+    public static int findNumber(int[] arr){
+        int count = 0;
+        for (int num : arr) {
+            if(checkEven(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+/* this method returns the number that are even digit*/
+//    public static ArrayList<Integer> findNumber(int[] arr){
+//        ArrayList<Integer> list = new ArrayList<>();
+//        int count = 0;
+//        for (int num : arr) {
+//            if(checkEven(num)){
+//                list.add(num);
+//            }
+//        }
+//        return new ArrayList<>(list);
+//    }
+
+    public static boolean checkEven(int num){
+        int count = 0;
+        while(num>0){
+            count++;
+            num/=10;
+        }
+        return count%2==0;
+    }
 }
