8dcd57010fc81224ed3e5acbbdea1979032e0020
Vigneshwaran-PS <psviki29@gmail.com>
2023-12-24 13:49:42 +0530

    CeilingNumber_35 is updated

--- LeetCode_Problems/CeilingNumber_35.java
+++ LeetCode_Problems/CeilingNumber_35.java
@@ -3,6 +3,16 @@ package Leetcode_and_technical_Programs.LeetCode_Problems;
 
 // Smallest element in array but greater than or equal to the target value
 // returns the index of the ceiling element
+
+// Example 1:
+//        Input: nums = [1,3,5,6], target = 5
+//        Output: 2
+// Example 2:
+//        Input: nums = [1,3,5,6], target = 2
+//        Output: 1
+// Example 3:
+//        Input: nums = [1,3,5,6], target = 7
+//        Output: 4
 public class CeilingNumber_35 {
     public static void main(String[] args) {
         System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 45));
