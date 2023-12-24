c33a6b13949608fb0fa209fe9fc09eb82e84a350
Vigneshwaran-PS <psviki29@gmail.com>
2023-12-24 13:48:09 +0530

    CeilingNumber_35 is created

---
+++ LeetCode_Problems/CeilingNumber_35.java
@@ -0,0 +1,34 @@
+package Leetcode_and_technical_Programs.LeetCode_Problems;
+
+
+// Smallest element in array but greater than or equal to the target value
+// returns the index of the ceiling element
+public class CeilingNumber_35 {
+    public static void main(String[] args) {
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 45));
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 7));
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 19));
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 14));
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 55));
+        System.out.println(ceiling(new int[]{2,6,7,10,15,19,23,45,68,98} , 1));
+    }
+
+    public static int ceiling(int[] arr , int target){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
+            int mid = start + (end -start) / 2;
+
+            if(target == arr[mid]){
+                return mid;
+            }
+            if(target > arr[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return start;
+    }
+}
